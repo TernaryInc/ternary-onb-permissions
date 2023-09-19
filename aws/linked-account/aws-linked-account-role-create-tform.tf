351a4856618a8e9d405b7ab19e4b458b1873406a
Gavin Celichowski <gavin@ternary.app>
2023-09-19 15:38:00 -0500

    remove scripts that have been moved to their own repo, reorg code, add linked account roles

---
+++ aws/linked-account/aws-linked-account-role-create-tform.tf
@@ -0,0 +1,96 @@
+locals {
+  ternary_service_account = "<INSERT_SERVICE_ACCOUNT>"
+  ternary_service_account_uid = "<INSERT_SERVICE_ACCOUNT_UID>"
+}
+resource "aws_iam_role" "ternary_cmp_linked_account_agent" {
+  name               = "ternary-cmp-linked-account-agent"
+  description        = "Permissions for Ternary to access your cloud for cost insights."
+  assume_role_policy = data.aws_iam_policy_document.ternary_cmp_linked_account_agent_assume_role.json
+}
+resource "aws_iam_role_policy" "ternary_cmp_linked_account_agent" {
+  name   = "ternary-cmp-linked-account-agent"
+  role   = aws_iam_role.ternary_cmp_linked_account_agent.name
+  policy = data.aws_iam_policy_document.ternary_cmp_linked_account_agent_permissions.json
+}
+data "aws_iam_policy_document" "ternary_cmp_linked_account_agent_permissions" {
+  version = "2012-10-17"
+  statement {
+    sid    = "AccountAccess"
+    effect = "Allow"
+    actions = [
+      "cloudwatch:ListTagsForResource",
+      "cloudwatch:ListMetrics",
+      "cloudwatch:ListMetricStreams",
+      "cloudwatch:GetMetricStatistics",
+      "cloudwatch:GetMetricData",
+      "cloudwatch:GetMetricStatistics",
+      "dynamodb:DescribeTable",
+      "dynamodb:ListTables",
+      "ec2:DescribeImages",
+      "ec2:DescribeInstances",
+      "ec2:DescribeRegions",
+      "ec2:DescribeReservedInstances",
+      "ec2:DescribeReservedInstancesModifications",
+      "ec2:DescribeSnapshots",
+      "ec2:DescribeVolumes",
+      "ec2:GetReservedInstancesExchangeQuote",
+      "ecs:DescribeClusters",
+      "ecs:DescribeContainerInstances",
+      "ecs:ListClusters",
+      "ecs:ListContainerInstances",
+      "elasticache:DescribeCacheClusters",
+      "elasticache:DescribeReservedCacheNodes",
+      "elasticache:ListTagsForResource",
+      "elasticmapreduce:DescribeCluster",
+      "elasticmapreduce:ListClusters",
+      "elasticmapreduce:ListInstances",
+      "rds:DescribeDBClusters",
+      "rds:DescribeDBInstances",
+      "rds:DescribeReservedDBInstances",
+      "rds:ListTagsForResource",
+      "redshift:DescribeClusters",
+      "redshift:DescribeReservedNodes",
+      "redshift:DescribeTags",
+      "savingsplans:DescribeSavingsPlans",
+      "ce:GetSavingsPlansPurchaseRecommendation"
+    ]
+    resources = ["*"]
+  }
+}
+data "aws_iam_policy_document" "ternary_cmp_linked_account_agent_assume_role" {
+  version = "2012-10-17"
+  statement {
+    sid     = "TernaryCMPLinkedAccountAgentAssumeRole"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = ["accounts.google.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "accounts.google.com:aud"
+      values   = local.ternary_service_account_uid
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "accounts.google.com:oaud"
+      values   = local.ternary_service_account
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "accounts.google.com:sub"
+      values   = local.ternary_service_account_uid
+    }
+    condition {
+      test     = "Null"
+      variable = "accounts.google.com:aud"
+      values   = [false]
+    }
+    condition {
+      test     = "Null"
+      variable = "accounts.google.com:oaud"
+      values   = [false]
+    }
+  }
+}
--- aws/scripts/env_setup.sh
+++
@@ -1,4 +0,0 @@
-virtualenv venv
-source venv/bin/activate
-
-pip3 install -r requirements.txt
