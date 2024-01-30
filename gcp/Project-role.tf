045fce0b2a04d22b9b352589db0723b1c352a3eb
Aaron Sproul <aaron@ternary.app>
2024-01-30 08:52:27 -0800

    Add Permissions for Cloud Run Service Cost Recommender
    This change adds the permissions necessary to get and list the
    [Cloud Run Service Cost Recommendations](https://cloud.google.com/run/docs/recommender).

--- gcp/Project-role.tf
+++ gcp/Project-role.tf
@@ -52,12 +52,14 @@ resource "google_organization_iam_custom_role" "ternary_service_agent" {
     "recommender.networkAnalyzerIpAddressInsights.list", # Unused Static IP Address Recommendations
     "recommender.resourcemanagerProjectUtilizationRecommendations.get", # [Future] Unattended Project Recommendations
     "recommender.resourcemanagerProjectUtilizationRecommendations.list", # [Future] Unattended Project Recommendations
+    "recommender.runServiceCostRecommendations.get", # Cloud Run Cost Recommendations
+    "recommender.runServiceCostRecommendations.list", # Cloud Run Cost Recommendations
     "recommender.spendBasedCommitmentInsights.get", # Spend Based Commitment Recommendations
     "recommender.spendBasedCommitmentInsights.list", # Spend Based Commitment Recommendations
-    "recommender.spendBasedCommitmentRecommendations.get", # [Future] Spend Based Commitment Recommendations
-    "recommender.spendBasedCommitmentRecommendations.list", # [Future] Spend Based Commitment Recommendations
-    "recommender.usageCommitmentRecommendations.get", # [Future] Usage Based Commitment Recommendations
-    "recommender.usageCommitmentRecommendations.list", # [Future] Usage Based Commitment Recommendations
+    "recommender.spendBasedCommitmentRecommendations.get", # Spend Based Commitment Recommendations
+    "recommender.spendBasedCommitmentRecommendations.list", # Spend Based Commitment Recommendations
+    "recommender.usageCommitmentRecommendations.get", # Usage Based Commitment Recommendations
+    "recommender.usageCommitmentRecommendations.list", # Usage Based Commitment Recommendations
     "resourcemanager.projects.get", # List Projects, used in multiple features
   ]
 }
