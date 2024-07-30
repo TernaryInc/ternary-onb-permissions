a86712b82258fa16663d236f2d66aa5adda11f90
Joshua Kwan <joshua.m.kwan@gmail.com>
2024-07-30 15:01:21 -0400

    Update aws/centralized-monitoring/variables.tf
    Co-authored-by: Aaron Sproul <aaron@ternary.app>

--- aws/centralized-monitoring/variables.tf
+++ aws/centralized-monitoring/variables.tf
@@ -6,4 +6,8 @@ variable "ternary_service_account_email" {
 variable "ternary_service_account_uid" {
   type        = string
   description = "Service Account Unique ID used by your Ternary tenant to access your cloud resources. Available in your Admin page."
+  validation {
+    condition     = length(regexall("[0-9]+", var.ternary_service_account_uid)) > 0
+    error_message = "The ternary_service_account_uid value must be numeric."
+  }
 }
